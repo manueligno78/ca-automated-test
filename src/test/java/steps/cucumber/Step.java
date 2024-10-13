6df847b022772c46ccdebdb79f54fac7c117b4e1
manueligno78 <manuelpintaldi@gmail.com>
2024-10-13 22:58:38 +0200

    Browserstack

--- src/test/java/steps/cucumber/Step.java
+++ src/test/java/steps/cucumber/Step.java
@@ -1,15 +1,20 @@
 package steps.cucumber;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.yaml.snakeyaml.Yaml;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,9 +22,6 @@ public class Step {
     protected static WebDriver driver;
     protected static WebDriverWait wait;
     public static void setUp() throws MalformedURLException {
-
-        // TODO: Browserstack variables should be get from a yaml specific file browserstack.yaml
-        // this will happen only if browser is "bs-chrome" or "bs-firefox", only if browser contains "bs-" prefix
         if (driver == null) {
             System.out.println("SETUP BROWSER");
             String browser = System.getenv("BROWSER");
@@ -27,32 +29,36 @@ public class Step {
             String hubPort = System.getenv("HUB_PORT");
             String hubUrl = (hubPort != null && !hubPort.isEmpty()) ? hubHost + ":" + hubPort : hubHost;
             System.out.println("HUB URL: " + hubUrl);
-            String browserstackUser = System.getenv("BROWSERSTACK_USERNAME");
-            String browserstackKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+            AbstractDriverOptions<?> options;
             if (browser == null) {
                 browser = "firefox-local";
             }
             if (browser.equalsIgnoreCase("chrome")) {
-                ChromeOptions options = new ChromeOptions();
-                if (browserstackUser != null && browserstackKey != null) {
-                    options.setCapability("browserstack.user", browserstackUser);
-                    options.setCapability("browserstack.key", browserstackKey);
-                    options.setCapability("build", "Docker example");
-                    options.setCapability("browserName", "chrome");
-                    options.setCapability("os", "OS X");
-                }
+                options = new ChromeOptions();
                 driver = new RemoteWebDriver(new URL("http://" + hubUrl + "/wd/hub"), options);
             } else if (browser.equalsIgnoreCase("firefox")) {
-                FirefoxOptions options = new FirefoxOptions();
-                if (browserstackUser != null && browserstackKey != null) {
-                    options.setCapability("browserstack.user", browserstackUser);
-                    options.setCapability("browserstack.key", browserstackKey);
-                    options.setCapability("build", "Docker example");
-                    options.setCapability("browserName", "firefox");
-                    options.setCapability("os", "OS X");
-                }
+                options = new FirefoxOptions();
                 driver = new RemoteWebDriver(new URL("http://" + hubUrl + "/wd/hub"), options);
-
+            } else if (browser.startsWith("browserstack")) {
+                Map<String, Object> browserstackConfig = loadBrowserStackConfig();
+                String browserstackUser = (String) browserstackConfig.get("userName");
+                String browserstackKey = (String) browserstackConfig.get("accessKey");
+                String browserstackBuild = (String) browserstackConfig.get("buildName");
+                String browserstackBrowserName = (String) browserstackConfig.get("browserName");
+                String browserstackOs = (String) browserstackConfig.get("os");
+                if (browserstackBrowserName.equalsIgnoreCase("chrome")) {
+                    options = new ChromeOptions();
+                } else if (browserstackBrowserName.equalsIgnoreCase("firefox")) {
+                    options = new FirefoxOptions();
+                } else {
+                    throw new IllegalArgumentException("Browser not supported: " + browserstackBrowserName);
+                }
+                options.setCapability("browserstack.user", browserstackUser);
+                options.setCapability("browserstack.key", browserstackKey);
+                options.setCapability("build", browserstackBuild);
+                options.setCapability("browserName", browserstackBrowserName);
+                options.setCapability("os", browserstackOs);
+                driver = new RemoteWebDriver(new URL("http://" + hubUrl + "/wd/hub"), options);
             } else if (browser.equalsIgnoreCase("chrome-local")) {
                 WebDriverManager.chromedriver().clearDriverCache().setup();
                 driver = new ChromeDriver();
@@ -65,6 +71,15 @@ public class Step {
             driver.manage().window().maximize();
             wait = new WebDriverWait(driver, 10);
             System.out.println("SETUP BROWSER DONE!");
+        }
+    }
+
+    private static Map<String, Object> loadBrowserStackConfig() {
+        Yaml yaml = new Yaml();
+        try (FileInputStream fis = new FileInputStream("browserstack.yml")) {
+            return yaml.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load BrowserStack configuration", e);
         }
     }
 
